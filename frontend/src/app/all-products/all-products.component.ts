b0479bb36dc9f14ff5762747052d178b038c09f8
martinorion <martinhox03@gmail.com>
2022-07-31 12:27:14 +0200

    Angular - edit grid styles

--- frontend/src/app/all-products/all-products.component.ts
+++ frontend/src/app/all-products/all-products.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {MainService} from "../main.service";
 import {AllProducts} from "../allProducts";
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
+import {HttpClient} from "@angular/common/http";
 
 
 @Component({
@@ -12,21 +13,40 @@ import {ActivatedRoute} from "@angular/router";
 export class AllProductsComponent implements OnInit {
 
   allProducts: AllProducts[];
-
+  allProducts1: AllProducts;
+  detailID: number;
+  dbImage: any;
+  postResponse: any;
+  skusimId: any;
 
   constructor(
     private mainService: MainService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private httpClient: HttpClient,
     ) {
-
+    this.detailID = +this.route.snapshot.paramMap.get('id');
   }
 
   ngOnInit(): void {
     this.mainService.getAllProducts().subscribe(data => {
       this.allProducts = data;
 
+
     } );
+
   }
 
+
+  viewImage(number: number) {
+    this.httpClient.get('http://localhost:8080/get/image/info/' + number)
+      .subscribe(
+        res => {
+          this.postResponse = res;
+          this.dbImage = 'data:image/jpeg;base64,' + this.postResponse.image;
+        }
+      );
+  }
 
 
 }
